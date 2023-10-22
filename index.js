191dc6f43298ffa20aaa1416301ee0f42af5a3ec
josephmangara <josephmangara2@gmail.com>
2023-10-22 14:17:07 +0300

    displayed data

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ function getMovies(){
 
 function displayMovieDetails(data){
     for (let details of data){
-        console.log(details)
+        console.log(details);
+        let list = document.getElementById("moviedetails");
+        let li = document.createElement('li');
+        li.textContent = details.title;
+        li.addEventListener("click", () => displayFilmDetails(details));
+        list.appendChild(li);
     }
+}
+
+// This function displays all movie details upon clicking the movie title.
+function displayFilmDetails(details){
+    purchaseTickets.innerHTML =`
+    <h2 id="moviedetails">${details.title}</h2>
+    <p>capacity: ${details.capacity}</p>
+    <p>description: ${details.description}</p>
+    <img src="${details.image}">
+    <p>runtime: ${details.runtime}</p>
+    <p>showtime: ${details.showtime}</p>
+    <p>tickets_sold: ${details.tickets_sold}</p>
+    <p></p>
+    `
 }
