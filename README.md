51c8429ce14fcb882de15d39cce4068d374dd8a6
josephmangara <josephmangara2@gmail.com>
2023-10-25 12:49:02 +0300

    complete

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # week-3-code-challenge
 Flatdango 
 title: Flatiron Movie Theater Tickets Purchase
+
 The application aims at providing users with detailed films information.
 It uses event handler functions to add interactivity and functionality to the flatdango webpage. 
 It fetches data from a json db server using GET request and displays the information using my javascript code.
@@ -10,8 +11,11 @@ The application relies on the displayMovieDetails function to display the data o
 The function creates new HTML elements that contain the details of the films and adds an event listener to display movie details upon clicking on a movie title.
 The javascript code includes a function and event listener that allows users to purchase tickets and determine whether there are available tickets.
 The code challenge adds a bonus deliverable that allows users to access every film by clicking on it and purchasing a ticket. 
+
 How to use the application;
 The films' menu is on the left side of the page.
 Users have to click on a movie title to see its poster, description, runtime, showtime, and capacity.
 To check the number of available tickets, the user needs to click on the Buy Ticket button which updates the sold tickets by one for each click. 
+
+
 
--- index.js
+++ index.js
@@ -8,9 +8,14 @@ function getMovies(){
     fetch("https://my-json-server.typicode.com/josephmangara/week-3-code-challenge/films")
     .then(res => res.json())
     .then(data => {
-        displayMovieDetails(data)
-    })
+        displayMovieDetails(data);
+//displaying details of the first film upon loading
+if (data.length > 0) {
+    let rand = Math.floor(Math.random() * data.length)
+    displayFilmDetails(data[rand]);
 }
+    })
+};
 //this function creates a list of all the films based on their title. 
 function displayMovieDetails(data){
     for (let details of data){
@@ -24,6 +29,7 @@ function displayMovieDetails(data){
     }
 }
 
+
 // This function displays all movie details upon clicking the movie title.
 function displayFilmDetails(details){
     let purchaseTickets = document.getElementById("purchaseTickets");
@@ -34,18 +40,25 @@ function displayFilmDetails(details){
       <p>Description: ${details.description}</p>
       <p>Runtime: ${details.runtime}</p>
       <p>Showtime: ${details.showtime}</p>
-      <p>Capacity: ${details.capacity}</p>
-      <p>Tickets_sold: ${details.tickets_sold}</p>
+      <p id="me">available tickets: ${details.capacity - details.tickets_sold}
       <button id="buyTicket">Buy Ticket</button>
       <p id="soldOutMessage" style="color: brown;"></p>
     </ul>
     `;
 // the event listener updates the tickets counter and prints out "sold out" when the tickets are exhausted.
     let buyTicketButton = document.getElementById("buyTicket");
+    let availableTickets = document.getElementById("availableTickets");
     buyTicketButton.addEventListener("click", () => {
         if(details.tickets_sold < details.capacity){
-        return details.tickets_sold++;
+        details.tickets_sold++;
+        availableTickets.textContent = `Available tickets: ${details.capacity - details.tickets_sold}`;
+        
        }else if(details.tickets_sold === details.capacity){
-        soldOutMessage.textContent = "Sold out!";}
+        soldOutMessage.textContent = "Sold out!";
+    }
     })
 };
+
+
+// <p>Tickets_sold: ${details.tickets_sold}</p>
+// <p>Capacity: ${details.capacity}</p>
